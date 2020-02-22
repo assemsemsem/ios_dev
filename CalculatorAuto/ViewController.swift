d44a46d9e8ccf8d350b3bbca0f2849a5170248cb
assemsemsem <assemsarsenbay08@gmail.com>
2020-02-22 17:30:45 +0600

    Initial Commit

---
+++ CalculatorAuto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorAuto
+//
+//  Created by Assem on 2/22/20.
+//  Copyright © 2020 Assem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
